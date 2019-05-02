07aaccbf261904d93d0f2b54b6f1f01da54f4a8f
Daniel Silva <djs@cin.ufpe.br>
2019-05-02 18:54:00 -0300

    adição do arquivo PÁGINA EM CONSTRUÇÃO

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   resources :professional_users
   resources :normal_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static_pages#index"
+  root to: 'static_pages#index'
   get 'cadastro_cliente', to: 'normal_users#new'
   get 'cadastro_profissional', to: 'professional_users#new'
   get 'entrar', to: 'sessions#new'
   post 'entrar', to: 'sessions#create'
+  get 'temp', to: 'static_pages#temp'
 end 
