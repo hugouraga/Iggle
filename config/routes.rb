79356ddc70c765ef4d9cb398ea4485915aa5e3c6
myllenaalves <myllenaalves971@gmail.com>
2019-05-05 00:58:49 -0300

    Adicionando primeira parte do SAC

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   resources :services
+  resources :contacts, only: [:new, :create]
   get 'sessions/new'
   resources :professional_users
   resources :normal_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#index'
+  root to: "static_pages#index"
   get 'cadastro_cliente', to: 'normal_users#new'
   get 'cadastro_profissional', to: 'professional_users#new'
-  get 'login_cliente', to: 'login_cliente#new'
-  post 'login_cliente', to: 'login_cliente#create'
-  get 'login_profissional', to: 'login_profissional#new'
-  post 'login_profissional', to: 'login_profissional#create'
-  get 'entrar', to: 'login_users#new'
-  #post 'entrar', to:  'login_users#new'
-  get 'temp', to: 'static_pages#temp'
-end 
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+end
