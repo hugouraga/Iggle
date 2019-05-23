a028628fcadf2b549c70dc565c64075ed2e16173
MyllenaAlmeida <roberta201016@hotmail.com>
2019-05-22 22:22:45 -0300

    visualização de serviços

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_020138) do
+ActiveRecord::Schema.define(version: 2019_05_13_003501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_020138) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "subject"
-    t.text "message"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
