f077240b09723434fc3549fdd39673c2a5369cb0
myllenaalves <myllenaalves971@gmail.com>
2019-05-26 17:14:28 -0300

    Adicionando atributos id e name nos campos

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,8 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
+    if user_signed_in
+      @contact = Contact.new
+    end
   end
 
   def create
@@ -10,6 +12,13 @@ class ContactsController < ApplicationController
       flash[:success] = 'Obrigada pela sua mensagem! Nós retornaremos em breve'
     else
       flash.now[:error] = 'Preencha os campos corretamente '
+    end
+  end
+  private
+  def require_logged_in_user
+    unless user_signed_in?
+      flash[:danger] = 'Área restrita. Por favor, realize o login.'
+      redirect_to entrar_url
     end
   end
 end
