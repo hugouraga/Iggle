77ef1549abe24cbe6d2b4c9719bd4b1f72f937d5
myllenaalves <myllenaalves971@gmail.com>
2019-05-29 14:46:32 -0300

    Deixando o campo complemento não obrigatório

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   validates :name, presence: true, length: { maximum: 500 }
   validates :number, presence: true, length: { maximum: 500 }
-  validates :description, presence: true, length: { maximum: 500 }
+  validates :description, presence: false, length: { maximum: 500 }
   validates :postal_code, presence: true, length: { maximum: 500 }
   validates :district, presence: true, length: { maximum: 500 }
   validates :city, presence: true, length: { maximum: 500 }
