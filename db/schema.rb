c55a2bcba88cbaedc73a63cca97fec6a652cc26a
hugo uraga <hugouraga61@gmail.com>
2019-06-16 03:33:56 -0300

    testes BDD - Alteração

--- db/schema.rb
+++ db/schema.rb
@@ -50,11 +50,48 @@ ActiveRecord::Schema.define(version: 2019_06_15_162548) do
     t.index ["professional_user_id"], name: "index_addresses_on_professional_user_id"
   end
 
+  create_table "cliente", primary_key: "email", id: :string, limit: 80, force: :cascade do |t|
+    t.string "senha", limit: 80, null: false
+    t.string "nome", limit: 80, null: false
+    t.string "telefone", limit: 80
+    t.decimal "reputacao"
+  end
+
+  create_table "cliente_avaliado", primary_key: ["email_cliente", "data_avaliacao", "hora", "email_avaliador"], force: :cascade do |t|
+    t.string "email_cliente", limit: 80, null: false
+    t.integer "estrelas", null: false
+    t.date "data_avaliacao", null: false
+    t.string "hora", limit: 20, null: false
+    t.string "email_avaliador", limit: 80, null: false
+    t.string "nome_servico", limit: 80, null: false
+  end
+
+  create_table "consulta_marcada", primary_key: ["data_consulta", "hora", "email_profissional", "email_cliente"], force: :cascade do |t|
+    t.date "data_consulta", null: false
+    t.string "hora", limit: 20, null: false
+    t.string "nome_servico", limit: 80, null: false
+    t.string "email_profissional", limit: 80, null: false
+    t.string "email_cliente", limit: 80, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "horario_atendimento", primary_key: ["email", "horario"], force: :cascade do |t|
+    t.string "email", limit: 80, null: false
+    t.string "horario", limit: 20, null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -92,8 +129,40 @@ ActiveRecord::Schema.define(version: 2019_06_15_162548) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "professor_esportes", primary_key: "email", id: :string, limit: 80, force: :cascade do |t|
+    t.string "confef", limit: 13, null: false
+  end
+
+  create_table "profissional", primary_key: "email", id: :string, limit: 80, force: :cascade do |t|
+    t.string "senha", limit: 80, null: false
+    t.string "nome", limit: 80, null: false
+    t.decimal "reputacao"
+    t.string "telefone", limit: 80, null: false
+    t.string "descricao", limit: 4000
+    t.string "facebook", limit: 80
+    t.string "instagram", limit: 80
+    t.string "cep", limit: 8, null: false
+    t.string "rua", limit: 80, null: false
+    t.integer "numero", null: false
+    t.string "email_convidante", limit: 80
+  end
+
+  create_table "profissional_avaliado", primary_key: ["email_profissional", "data_avaliacao", "hora", "email_avaliador"], force: :cascade do |t|
+    t.string "email_profissional", limit: 80, null: false
+    t.integer "estrelas", null: false
+    t.string "comentario", limit: 4000
+    t.date "data_avaliacao", null: false
+    t.string "hora", limit: 20, null: false
+    t.string "email_avaliador", limit: 80, null: false
+    t.string "nome_servico", limit: 80, null: false
+  end
+
+  create_table "profissional_ed_fisica", primary_key: "email", id: :string, limit: 80, force: :cascade do |t|
+    t.string "confef", limit: 13, null: false
+  end
+
   create_table "schedules", force: :cascade do |t|
-    t.string "day", null: false
+    t.string "day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "morning", default: "false", null: false
@@ -117,9 +186,27 @@ ActiveRecord::Schema.define(version: 2019_06_15_162548) do
     t.index ["professional_user_id"], name: "index_services_on_professional_user_id"
   end
 
+  create_table "servico", primary_key: ["nome", "email_profissional"], force: :cascade do |t|
+    t.string "nome", limit: 80, null: false
+    t.string "descricao", limit: 4000
+    t.decimal "valor", null: false
+    t.string "duracao", limit: 20, null: false
+    t.string "email_profissional", limit: 80, null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "professional_users"
+  add_foreign_key "cliente_avaliado", "cliente", column: "email_cliente", primary_key: "email", name: "cliente_avaliado_cliente_fk"
+  add_foreign_key "cliente_avaliado", "profissional", column: "email_avaliador", primary_key: "email", name: "cliente_avaliado_avaliador_fk"
+  add_foreign_key "consulta_marcada", "cliente", column: "email_cliente", primary_key: "email", name: "consulta_marcada_cliente_fk"
+  add_foreign_key "consulta_marcada", "profissional", column: "email_profissional", primary_key: "email", name: "consulta_marcada_profissional_fk"
+  add_foreign_key "horario_atendimento", "profissional", column: "email", primary_key: "email", name: "horario_atendimento_fk"
+  add_foreign_key "professor_esportes", "profissional", column: "email", primary_key: "email", name: "professor_esportes_fk"
+  add_foreign_key "profissional_avaliado", "cliente", column: "email_avaliador", primary_key: "email", name: "profissional_avaliado_avaliador_fk"
+  add_foreign_key "profissional_avaliado", "profissional", column: "email_profissional", primary_key: "email", name: "profissional_avaliado_profissional_fk"
+  add_foreign_key "profissional_ed_fisica", "profissional", column: "email", primary_key: "email", name: "profissional_ed_fisica_fk"
   add_foreign_key "schedules", "addresses"
   add_foreign_key "services", "addresses"
   add_foreign_key "services", "professional_users"
+  add_foreign_key "servico", "profissional", column: "email_profissional", primary_key: "email", name: "servico_fk"
 end
---
+++ features/steps_definitions.rb/alterar_conta_profissional.rb
@@ -0,0 +1,36 @@
+Dado("que estou logado em meu perfil") do
+end
+
+Dado("clico na aba sidebar") do
+    find('[id=openbtn]').click
+end
+
+Dado("clico na sessão {string}") do |string|
+    find('[id=Conta]').click
+    save_and_open_page
+end
+
+Então("eu deveria está na página de alteração de conta do profissional") do
+    visit edit_professional_user_path
+end
+
+Quando("alterar os dados necessários do formulário profissional") do
+    fill_in("profissional_name", with: 'teste')
+    fill_in("profissional_email", with: 'teste.nutri@gmail.com')
+    fill_in("profissional_phone", with: 988888888)
+    fill_in("profissional_document", with: 847984749874894)
+    fill_in("profissional_instagram", with: "teste.nutri")
+    fill_in("profissional_facebook", with: "teste.nutri")
+    fill_in("profissional_description", with: "Nutrição Funcional - Saúde & Bem estar")
+    fill_in("profissional_password", with: '123456789')
+    fill_in("profissional_passwordc", with: '123456789')
+end
+
+Quando("clicar no botão {string}") do |string|
+    find('[name=Salvar]').click
+    save_and_open_page
+end
+
+Então("meus dados são atualizados com sucesso") do
+    expect(page).to have_content('Dados atualizados com sucesso!')
+end
